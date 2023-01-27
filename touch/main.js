0a6b736858e196e5ff0feb92b452462dbf905b77
YUPENG123 <yupengdongguan@hotmail.com>
2023-01-28 04:57:09 +0800

    chi

--- touch/main.js
+++ touch/main.js
@@ -18,6 +18,6 @@ function create() {
 function render() {
     // 调试信息
     game.debug.pointer(game.input.mousePointer);
-    game.debug.pointer(game.input.pointer1);
-    game.debug.pointer(game.input.pointer2);
+    //game.debug.pointer(game.input.pointer1);
+    //game.debug.pointer(game.input.pointer2);
 }
