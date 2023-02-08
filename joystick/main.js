cb7942d9c5843b01ffc782f331a91a5f0064de65
yp <yp523ne@163.com>
2023-02-09 00:16:02 +0800

    vhh high

--- joystick/main.js
+++ joystick/main.js
@@ -5,14 +5,16 @@ var PhaserGame = function () {
 };
 
 PhaserGame.prototype = {
-init:function(){
-console.log('init');
-},
-preload:function(){
-console.log('preload');
-},
-
+    init: function () {
+        console.log('init');
+    },
+    preload: function () {
+        console.log('preload');
+    },
+    create: function () {
+        console.log('create');
+    },
 };
 
-game.state.add('game',PhaserGame,true);
+game.state.add('game', PhaserGame, true);
 
