a18a80c21aa57095d8d06ecf259392d7abd10dec
YUPENG123 <yupengdongguan@hotmail.com>
2023-02-09 01:16:50 +0800

    vic

--- joystick/main.js
+++ joystick/main.js
@@ -1,26 +1,23 @@
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, "phaser-example");
 
-    var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example');
+var PhaserGame = function () {};
 
-    var PhaserGame = function () {
+PhaserGame.prototype = {
+  init: function () {
+    console.log("init");
+  },
+  preload: function () {
+    console.log("preload");
+  },
+  create: function () {
+    console.log("create");
+  },
+  pressButtonA: function () {
+    console.log("buttonA");
+  },
+  pressButtonB: function () {
+    console.log("buttonB");
+  },
+};
 
-    };
-
-    PhaserGame.prototype = {
-        init: function () {
-            console.log('init');
-        },
-        preload: function () {
-            console.log('preload');
-        },
-        create: function () {
-            console.log('create');
-        },
-        pressButtonA: function () {
-            console.log('buttonA');
-        },
-        pressButtonB: function () {
-            console.log('buttonB');
-        },
-    };
-
-    game.state.add('game', PhaserGame, true);
+game.state.add("game", PhaserGame, true);
