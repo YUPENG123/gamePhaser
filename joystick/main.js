67e4fad6d3e460e947a56414a2662321dc2f0d55
yp <yp523ne@163.com>
2023-02-09 16:09:24 +0800

    sgsdg

--- joystick/main.js
+++ joystick/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, "phaser-example");
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, "phaser-example");
 
 var PhaserGame = function () {
   this.sprite;
