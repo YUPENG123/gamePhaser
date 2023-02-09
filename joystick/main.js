631e657da59676cc73bf7d00af10b180419d8771
YUPENG123 <yupengdongguan@hotmail.com>
2023-02-09 20:17:34 +0800

    chef

--- joystick/main.js
+++ joystick/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, "phaser-example");
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example');
 
 var PhaserGame = function () {
   this.sprite;
@@ -15,38 +15,38 @@ PhaserGame.prototype = {
     this.physics.startSystem(Phaser.Physics.ARCADE);
   },
   preload: function () {
-    this.load.image("bg", "assets/space1.png");
-    this.load.image("ball", "assets/beball1.png");
+    this.load.image('bg', 'assets/space1.png');
+    this.load.image('ball', 'assets/beball1.png');
     this.load.atlas(
-      "arcade",
-      "assets/arcade-joystick.png",
-      "assets/arcade-joystick.json"
+      'arcade',
+      'assets/arcade-joystick.png',
+      'assets/arcade-joystick.json'
     );
   },
   create: function () {
-    this.add.image(0, 0, "bg");
-    this.sprite = this.add.sprite(400, 200, "ball");
+    this.add.image(0, 0, 'bg');
+    this.sprite = this.add.sprite(400, 200, 'ball');
 
     this.pad = this.game.plugins.add(Phaser.VirtualJoyStick);
-    this.stick = this.pad.addStick(0, 0, 200, "arcade");
+    this.stick = this.pad.addStick(0, 0, 200, 'arcade');
     this.stick.alignBottomLeft();
     this.console.log(this.pad);
     console.log(stick);
     console.log(sprite);
   },
   pressButtonA: function () {
-    console.log("buttonA");
+    console.log('buttonA');
   },
   pressButtonB: function () {
-    console.log("buttonB");
+    console.log('buttonB');
   },
   pressButtonC: function () {
-    console.log("buttonC");
+    console.log('buttonC');
   },
   update: function () {
-    console.log("update");
+    console.log('update');
   },
 };
 
-game.state.add("game", PhaserGame, true);
+game.state.add('game', PhaserGame, true);
 //console.log(PhaserGame);
